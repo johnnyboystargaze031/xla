f35899acb9a71702b2f44ea7852bf2c09839b3f3
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2024-02-14 18:37:28 -0800

    format

--- test/test_fori_loop_with_while_loop_simple_add_dispatch_in_torch.py
+++ test/test_fori_loop_with_while_loop_simple_add_dispatch_in_torch.py
@@ -11,17 +11,19 @@ from torch_xla.experimental.fori_loop import while_loop
 class WhileLoopTest(unittest.TestCase):
 
   def test_while_loop_tpu(self):
+
     def cond_fn(counter, x):
       return counter < xb.Op.scalar(x.builder(), 10, dtype=xb.Type.S32)
 
     def body_fn(counter, x):
-      next_counter = counter + xb.Op.scalar(counter.builder(), 1, dtype=xb.Type.S32)
+      next_counter = counter + xb.Op.scalar(
+          counter.builder(), 1, dtype=xb.Type.S32)
       x = x + xb.Op.scalar(x.builder(), 1, dtype=xb.Type.S32)
       return xb.Op.tuple((next_counter, x))
 
     device = xm.xla_device()
     x = torch.ones(1, dtype=torch.int, device=device)
-    res = while_loop(cond_fn, body_fn, (x, ))
+    res = while_loop(cond_fn, body_fn, (x,))
     print("while_loop result: ", res)
     expected = torch.tensor(11, dtype=torch.int, device=device)
     print("expected result: ", expected)
